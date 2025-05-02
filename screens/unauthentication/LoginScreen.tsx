3441f0085a7ef6517cae38442e6a92d4b9d9cc39
hampus-code <hampusss96@gmail.com>
2025-05-02 17:49:11 +0200

    Created setup for a very basic LoginScreen, just to make it possible to actually login

--- screens/unauthentication/LoginScreen.tsx
+++ screens/unauthentication/LoginScreen.tsx
@@ -1,9 +1,37 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function LoginScreen() {
+  const [userInput, setUserInput] = useState("");
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login Screen</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setUserInput}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setUserInput}
+        placeholder="Password"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textInput: {
+    borderWidth: 1,
+    width: 250,
+    borderRadius: 10,
+    marginTop: 10
+  }
+});
